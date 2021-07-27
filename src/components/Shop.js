2c38fca2d21fcac173f0b55057d17c5f3c6816f4
Kevin <kfachas@gmail.com>
2021-07-27 17:03:11 +0200

    add modif #2 shop

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,47 +1,62 @@
 const Shop = ({ values, setValues }) => {
   return (
-    <div className="shop">
-      <button className="valid">Valider mon panier</button>
+    <div
+      className="shop"
+      style={{
+        height: values.total === 0 && "150px",
+      }}
+    >
+      <button id="btn" className={values.total === 0 ? "notvalid" : "valid"}>
+        Valider mon panier
+      </button>
 
       {Number(values.total) === 0 ? (
         <span>Le panier est vide</span>
       ) : (
-        <ul>
-          {values.listId.map((elem, index) => {
-            const obj = { ...values };
-            console.log(elem);
-            return (
-              obj.menu[elem].counter !== 0 && (
-                <li key={index}>
-                  <button
-                    onClick={() => {
-                      obj.menu[elem].counter -= 1;
-                      obj.total -= obj.menu[elem].price;
-                      setValues(obj);
-                    }}
-                  >
-                    -
-                  </button>
-                  {values.menu[elem].counter}
-                  <button
-                    onClick={() => {
-                      obj.menu[elem].counter += 1;
-                      obj.total += obj.menu[elem].price;
-                      setValues(obj);
-                    }}
-                  >
-                    +
-                  </button>
-                  {values.menu[elem].title}
-                  {values.menu[elem].price} €
-                </li>
-              )
-            );
-          })}
-          <li>Sous-total: {values.total.toFixed(2)}</li>
-          <li>Frais de livraison : 2,50€ </li>
-          <span>Total :{(values.total + 2.5).toFixed(2)} €</span>
-        </ul>
+        <>
+          <ul>
+            {values.listId.map((elem, index) => {
+              const obj = { ...values };
+              console.log(elem);
+              return (
+                obj.menu[elem].counter !== 0 && (
+                  <li key={index}>
+                    <button
+                      onClick={() => {
+                        obj.menu[elem].counter -= 1;
+                        obj.total -= obj.menu[elem].price;
+                        setValues(obj);
+                      }}
+                    >
+                      -
+                    </button>
+                    {values.menu[elem].counter}
+                    <button
+                      onClick={() => {
+                        obj.menu[elem].counter += 1;
+                        obj.total += obj.menu[elem].price;
+                        setValues(obj);
+                      }}
+                    >
+                      +
+                    </button>
+                    {values.menu[elem].title}
+                    {values.menu[elem].price} €
+                  </li>
+                )
+              );
+            })}
+          </ul>
+          <div>
+            <div>
+              <span>Sous-total: {values.total.toFixed(2)}</span>
+              <span>Frais de livraison : 2,50€</span>
+            </div>
+            <div>
+              <span>Total :{(values.total + 2.5).toFixed(2)} €</span>
+            </div>
+          </div>
+        </>
       )}
     </div>
   );
